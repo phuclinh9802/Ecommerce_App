a1af7c3bbfe6d6ee1f618e32264f4a6229124d1e
Phillip Nguyen <phucnguyen290198@gmail.com>
2022-06-14 00:33:36 +0700

    Sign Up/Sign In modals linking to each other

--- src/components/Authenticate/Login.js
+++ src/components/Authenticate/Login.js
@@ -1,82 +1,125 @@
-import React, { Component, useState } from 'react';
-import { Navigate, Link } from 'react-router-dom'
-import isAuthenticated from '../../lib/authenticate'
-import { Modal, ModalOverlay, ModalBody, ModalContent, ModalHeader, ModalFooter, ModalCloseButton } from '@chakra-ui/react'
-import { useDisclosure, Button, FormLabel, FormControl, Input } from '@chakra-ui/react';
-import { SIGN_IN } from '../../constants/auth';
+import React, { Component, useState, forwardRef } from "react";
+import { Navigate, Link } from "react-router-dom";
+import isAuthenticated from "../../lib/authenticate";
+import {
+  Modal,
+  ModalOverlay,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import {
+  useDisclosure,
+  Button,
+  FormLabel,
+  FormControl,
+  Input,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import { SIGN_IN } from "../../constants/auth";
 
 const Login = (props) => {
-    const [loggedIn, setLoggedIn] = useState(isAuthenticated())
-    const { isOpen, onClose } = props;
+  const [loggedIn, setLoggedIn] = useState(isAuthenticated());
+  const { isOpen, onClose, onOpen } = props;
+  //   const { isOpen, onClose, onOpen } = useDisclosure();
 
-    const initialRef = React.useRef()
-    const finalRef = React.useRef()
+  const initialRef = React.useRef();
+  const finalRef = React.useRef();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        e.stopPropagation()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-        let form = e.target
-        let formData = new FormData(form)
-        let params = new URLSearchParams()
-        params.append('username', formData.get('username'))
-        params.append('password', formData.get('password'))
-        params.append('remember', formData.get('remember'))
+    let form = e.target;
+    let formData = new FormData(form);
+    let params = new URLSearchParams();
+    params.append("username", formData.get("username"));
+    params.append("password", formData.get("password"));
+    params.append("remember", formData.get("remember"));
 
-        // POST request to server
-        fetch('/api/login', {
-            method: 'POST',
-            body: params
-        }).then((res) => {
-            return res.json()
-        }).then(data => {
-            localStorage.setItem('token', data.token)
-            setLoggedIn(true)
-        }).catch((err) => {
-            console.error(err)
-        })
-    }
+    // POST request to server
+    fetch("/api/login", {
+      method: "POST",
+      body: params,
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        setLoggedIn(true);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-    // if (loggedIn) {
-    //     return (
-    //         <Navigate to={{ pathname: '/', state: { from: this.props.location } }}
-    //         />
-    //     )
-    // }
+  // if (loggedIn) {
+  //     return (
+  //         <Navigate to={{ pathname: '/', state: { from: this.props.location } }}
+  //         />
+  //     )
+  // }
 
-    // else {
-    return (
-        <>
-            <Modal initialFocusRef={initialRef} finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <form onSubmit={handleSubmit}>
-                    <ModalContent>
-                        <ModalHeader>Login</ModalHeader>
-                        <ModalBody pb={6}>
-                            <ModalCloseButton />
-                            <FormControl isRequired>
-                                <FormLabel>Username</FormLabel>
-                                <Input ref={initialRef} placeholder="Username" />
-                            </FormControl>
-                            <FormControl isRequired>
-                                <FormLabel>Password</FormLabel>
-                                <Input type="password" placeholder="Password" required />
-                            </FormControl>
-                        </ModalBody>
-                        <ModalFooter>
-                            <Link to="/dashboard">
-                                <Button type="submit" variantColor="teal" variant="outline" width="full" mr={3}>
-                                    Login
-                                </Button>
-                            </Link>
-                        </ModalFooter>
-                    </ModalContent>
-                </form>
-            </Modal>
-        </>
-
-    )
-}
+  // else {
+  return (
+    <>
+      {/* <Button colorScheme="red" onClick={onOpen}>
+        {SIGN_IN}
+      </Button> */}
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <form onSubmit={handleSubmit}>
+          <ModalContent>
+            <ModalHeader>Login</ModalHeader>
+            <ModalBody pb={6}>
+              <ModalCloseButton />
+              <FormControl isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input ref={initialRef} placeholder="Username" />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" placeholder="Password" required />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Link to="/login">
+                <ChakraLink
+                  mr={10}
+                  onClick={() => {
+                    onClose();
+                    onOpen();
+                  }}
+                >
+                  Already have an account? Click here
+                </ChakraLink>
+              </Link>
+              <Link to="/dashboard">
+                <Button
+                  type="submit"
+                  variantColor="teal"
+                  variant="outline"
+                  width="full"
+                  mr={3}
+                >
+                  Login
+                </Button>
+              </Link>
+            </ModalFooter>
+          </ModalContent>
+        </form>
+      </Modal>
+    </>
+  );
+};
 // }
 
 export default Login;
--- src/components/Authenticate/Register.js
+++ src/components/Authenticate/Register.js
@@ -1,10 +1,143 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import {
+  useDisclosure,
+  Button,
+  FormLabel,
+  FormControl,
+  Input,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
 
-const Register = () => {
-    return (
-        <>
-        </>
-    )
-}
+import { Link } from "react-router-dom";
+import Login from "./Login";
+import { SIGN_UP } from "../../constants/auth";
 
-export default Register
+const Register = (props) => {
+  // initial state:
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+    error: {},
+  });
+
+  const { isOpen, onClose, onOpen } = props;
+  //   const { isOpen, onClose, onOpen } = useDisclosure();
+  //   const {
+  //     isOpen: isLoginOpen,
+  //     onOpen: onLoginOpen,
+  //     onClose: onLoginClose,
+  //   } = useDisclosure();
+  const initialRef = React.useRef();
+  const finalRef = React.useRef();
+
+  const inputRef = React.createRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      inputRef.button.current.click();
+    }, 5000);
+  }, []);
+
+  const fun = () => {
+    console.log("Hello!");
+  };
+
+  const handleChange = (e) => {
+    setUser({ [e.target.id]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const newUser = {
+    name: user.name,
+    email: user.email,
+    password: user.password,
+    password2: user.password2,
+  };
+  return (
+    <>
+      {/* <Button colorScheme="teal" onClick={onOpen}>
+        {SIGN_UP}
+      </Button> */}
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        size="xl"
+      >
+        <ModalOverlay />
+        <form onSubmit={handleSubmit}>
+          <ModalContent>
+            <ModalHeader>Login</ModalHeader>
+            <ModalBody pb={6}>
+              <ModalCloseButton />
+              <FormControl isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input ref={initialRef} placeholder="Username" />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>First Name</FormLabel>
+                <Input ref={initialRef} placeholder="First Name..." />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Last Name</FormLabel>
+                <Input ref={initialRef} placeholder="Last Name..." />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" placeholder="Password" />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Confirm Password</FormLabel>
+                <Input type="password" placeholder="Confirm Password" />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Link to="/login">
+                <ChakraLink
+                  mr={10}
+                  onClick={() => {
+                    onClose();
+                    onOpen();
+                  }}
+                >
+                  Already have an account? Click here
+                </ChakraLink>
+              </Link>
+              <Link to="/">
+                <Button
+                  type="submit"
+                  variantColor="teal"
+                  variant="outline"
+                  width="full"
+                  mr={3}
+                  onClick={() => {
+                    onClose();
+                    console.log("success!");
+                  }}
+                >
+                  Register
+                </Button>
+              </Link>
+            </ModalFooter>
+          </ModalContent>
+        </form>
+      </Modal>
+    </>
+  );
+};
+
+export default Register;
