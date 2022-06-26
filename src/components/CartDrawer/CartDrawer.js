b764818c7689895ca4b7fb58ec6be0dae121733d
Phuc Nguyen <anicedream2002@gmail.com>
2022-06-26 22:05:32 +0700

    display flex grid for each item inside drawer

--- src/components/CartDrawer/CartDrawer.js
+++ src/components/CartDrawer/CartDrawer.js
@@ -6,6 +6,11 @@ import {
   DrawerBody,
   DrawerHeader,
   DrawerCloseButton,
+  Grid,
+  GridItem,
+  Flex,
+  Center,
+  Divider,
 } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
@@ -24,14 +29,30 @@ const CartDrawer = ({ onClose, isOpen }) => {
         <DrawerCloseButton />
         <DrawerHeader borderBottomWidth='1px'>Shopping Cart</DrawerHeader>
         <DrawerBody>
-          {items.map((el) => {
-            return (
-              <>
-                <p>{el.name}</p>
-                <img src={el.image} />
-              </>
-            )
-          })}
+          <Flex flexDirection={'column'} gap={5}>
+            {items.map((el) => {
+              return (
+                <>
+                  <Grid alignItems={'center'} justifyContent={'center'} gap={2} gridTemplateColumns={'1fr 1fr 50px'}>
+                    <GridItem >
+                      <p>{el.name}</p>
+                    </GridItem>
+                    <GridItem>
+                      <Center>
+                        <img src={el.image} width="180px" height="180px" />
+                      </Center>
+                    </GridItem>
+                    <GridItem>
+                      <Center>
+                        <Button colorScheme={'red'} variantColor={'red'} variant="ghost" size="sm" borderRadius={50}>X</Button>
+                      </Center>
+                    </GridItem>
+                  </Grid>
+                  <Divider width={'100%'} />
+                </>
+              )
+            })}
+          </Flex>
 
         </DrawerBody>
       </DrawerContent>
